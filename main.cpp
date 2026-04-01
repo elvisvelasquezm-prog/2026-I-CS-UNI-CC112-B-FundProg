87c85e8132058f6cc808d3139892b0feaec5d537
elvisvelasquezm-prog <elvis.velasquez.m@uni.pe>
2026-04-01 15:01:33 -0500

    primer prog

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream> // cout 
-#include "arit.h"   // suma, resta
+#include "arit.h"
+using namespace std;   // suma, resta
 
 // g++ -std=c++2b main.cpp arit.cpp -o main
 // El main() debe ser muy pero muy pequeñito
@@ -9,9 +10,13 @@ int main() {
     int a = 50;
     int b = 20;
     int c = suma(a, b);
-    std::cout << c << std::endl;
-    //std::cout << "La suma de " << a << " y " << b << " es " << c << std::endl;
-    c = resta(a, b);
-    std::cout << c << std::endl;
+    cout << c << endl;
+    cout << "La suma de " << a << " y " << b << " es " << c << endl;
+    int d = resta(a, b);
+    cout << d <<endl;
+    cout << "La resta de " << a << " y " << b << " es " << d << endl;
+    int e= producto(a,b);
+    cout<<e<<endl;
+    cout << "el producto de " << a << " y " << b << " es " << e << endl;
     return 0;
-}
+} 
